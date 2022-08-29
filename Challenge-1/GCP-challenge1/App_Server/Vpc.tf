ab0128ffdc6fa4da545cbcaa05cf87421fab1fe2
Shiva123-web <79076316+Shiva123-web@users.noreply.github.com>
2022-08-29 16:39:43 +0530

    Update Vpc.tf

--- Challenge-1/GCP-challenge1/App_Server/Vpc.tf
+++ Challenge-1/GCP-challenge1/App_Server/Vpc.tf
@@ -19,3 +19,12 @@ resource "google_compute_subnetwork" "app_subnet1" {
   network       = google_compute_network.wp_vpc.id
   private_ip_google_access = true
 }
+
+resource "google_compute_network_peering" "peering1" {
+  depends_on = [
+    google_compute_network.wp_vpc
+  ]
+  name         = "peering1"
+  network      = google_compute_network.wp_vpc.id
+  peer_network = var.sql_vpc_id
+}
